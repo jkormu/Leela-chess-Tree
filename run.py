f573b04b31b9a9cf41be46ade062944313f8d759
jusufe <you@example.com>
2020-04-12 23:47:29 +0300

    open browser with delay

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import app
 import webbrowser
+from threading import Timer
+
+def open_browser():
+    webbrowser.open_new_tab("http://localhost:8050")
 
 if __name__ == '__main__':
-    webbrowser.open_new_tab("http://localhost:8050")
+    Timer(2, open_browser).start() #open browser with delay so server has time to start up
     app.run_server(debug=False, threaded=False)
