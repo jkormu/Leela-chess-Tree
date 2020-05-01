d052db43a3f637b4d299298cc8ae4c30f67c0f9b
jusufe <you@example.com>
2020-05-01 20:00:54 +0300

    axis title

--- heatmaps.py
+++ heatmaps.py
@@ -92,20 +92,35 @@ def heatmap_component():
     container.children = [container_left, container_right, dummy_spacer]
     return(container)
 
-#@app.callback(
-#    [Output('depth-selector', 'max'),
-#     Output('depth-selector', 'value')],
-#    [Input('slider1', 'value'),
-#     Input('move-table', 'active_cell'),
-#     Input('position-mode-selector', 'value')])
-#def update_depth_selector_max(slider_value, active_cell, position_mode):
-#    if position_mode == 'pgn':
-#        tree_data = tree_data_pgn
-#        game_data = game_data_pgn
-#    else:
-#        tree_data = tree_data_fen
-#        game_data = game_data_fen
-#    return()
+@app.callback(
+    [Output('depth-selector', 'max'),
+     Output('depth-selector', 'value')],
+    [Input('move-table', 'active_cell'),
+     Input('position-mode-selector', 'value')],
+    [State('depth-selector', 'value'),
+     State('depth-selector', 'max')])
+def update_depth_selector_max(active_cell, position_mode, selected_depths, current_max):
+    if active_cell is None or position_mode is None:
+        return(dash.no_update, dash.no_update)
+    selected_min_depth, selected_max_depth = selected_depths
+    if position_mode == 'pgn':
+        tree_data = tree_data_pgn
+        game_data = game_data_pgn
+    else:
+        tree_data = tree_data_fen
+        game_data = game_data_fen
+    position_id = game_data.get_position_id(active_cell['row'])
+    try:
+        new_max = max(tree_data.y_range[position_id])
+    except KeyError:
+        return(dash.no_update, dash.no_update)
+
+    if selected_max_depth == current_max or selected_max_depth > new_max:
+        selected_max_depth = new_max
+    selected_min_depth = min(selected_min_depth, selected_max_depth - 1)
+
+    selected_depths = [selected_min_depth, selected_max_depth]
+    return(new_max, selected_depths)
 
 @app.callback(
     Output('depth-filter-info', 'children'),
@@ -292,95 +307,3 @@ def destination_origin_figure(heatmap_type, heatmap_data, color_filter, piece_fi
     fig['layout'].update(layout)
     fig['layout']['autosize'] = True
     return(fig)
-
-a = """
-def update_heatmap_old(heatmap_type, position_mode, active_cell, active_tab, slider_value,
-                   color_filter, piece_filter, depth_filter):
-    depth_filter_min, depth_filter_max = depth_filter
-    #print('entered heatmap')
-    def key_filter(key):
-        if color_filter != 'both' and key[0] != color_filter:
-            return(False)
-        if piece_filter != 'all' and key[1] != piece_filter:
-            return(False)
-        if key[2] < depth_filter_min or key[2] >= depth_filter_max:
-            return(False)
-        return(True)
-
-
-    if heatmap_type is None or active_tab != 'heatmaps':
-        return(empty_figure())
-
-    start = time.time()
-    if position_mode == 'pgn':
-        tree_data = tree_data_pgn
-        game_data = game_data_pgn
-    else:
-        tree_data = tree_data_fen
-        game_data = game_data_fen
-    selected_row = active_cell['row']
-    position_id = game_data.get_position_id(selected_row)
-    #tree_data.calculate_heatmap_helpers(position_id)
-    if position_id in tree_data.G_dict:
-        try:
-            heatmap_data = tree_data.heatmap_data[position_id][slider_value]
-        except KeyError:
-            print('generating heat map data')
-            tree_data.calculate_heatmap_helpers(position_id)
-            heatmap_data = tree_data.heatmap_data[position_id][slider_value]
-    else:
-        return(empty_figure())
-
-    print('heatmap data retrieved in', time.time() - start)
-    start = time.time()
-    Zs = [heatmap_data[key][heatmap_type] for key in heatmap_data if key_filter(key)]
-    if Zs != []:
-        #print('Zs', Zs)
-        flat_Zs = [[element for row in z for element in row] for z in Zs]
-        #print('flat', flat_Zs)
-        z = list(sum(l) for l in zip(*flat_Zs))
-        #print('z', z)
-        z = [z[i * 8:(i + 1) * 8] for i in range(8)]
-    else:
-        z = [[0, 0, 0, 0, 0, 0, 0, 0] for _ in range(8)]
-    #print('z', z)
-    print('heatmap data processed in', time.time() - start)
-
-    fig = go.Figure(data=go.Heatmap(
-        z=z,
-        x=[0,1,2,3,4,5,6,7],
-        y=[0,1,2,3,4,5,6,7],
-        colorscale='Viridis',#'Cividis',#'Bluered',#'Inferno',#'Viridis',
-        xgap=2,
-        ygap=2,
-        zmin=0,
-        ),
-    )
-
-    layout = go.Layout(
-        autosize=False,
-        xaxis={'title': None,
-               'range': [-0.5, 7.5],
-               'zeroline': False,
-               'showgrid': False,
-               'scaleanchor':'y',
-               'constrain': 'domain',
-               'constraintoward': 'right',
-               'ticktext': [letter for letter in 'abcdefgh'],
-               'tickvals': [0,1,2,3,4,5,6,7]
-               },
-        yaxis={'title': None,
-               'range': [-0.5, 7.5],
-               'zeroline': False,
-               'showgrid': False,
-               'ticktext': [letter for letter in '12345678'],
-               'tickvals': [0, 1, 2, 3, 4, 5, 6, 7],
-               'constrain': 'domain'
-               # 'gridcolor': GRID_COLOR
-               },
-        margin={'t': 0, 'b': 0, 'r': 0, 'l': 0},
-    )
-
-    fig['layout'].update(layout)
-    fig['layout']['autosize'] = True
-    return(fig)"""
